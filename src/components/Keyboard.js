6ca8c826e68e86de0d5136306c27249af2353aed
sungwoncho <sungwon_cho@fursys.com>
2023-04-06 15:56:33 +0900

    키보드 배열 생성

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,14 +1,39 @@
+const keyboardArr1 = ['ㅂ', 'ㅈ', 'ㄷ', 'ㄱ', 'ㅅ', 'ㅛ', 'ㅕ', 'ㅑ'];
+const keyboardArr2 = ['ㅁ', 'ㄴ', 'ㅇ', 'ㄹ', 'ㅎ', 'ㅗ', 'ㅓ', 'ㅏ', 'ㅣ'];
+const keyboardArr3 = ['ㅋ', 'ㅌ', 'ㅊ', 'ㅍ', 'ㅠ', 'ㅜ', 'ㅡ'];
+
 const Keyboard = ({ handleKeyboardClick }) => {
+
+    const KeyboardBtn = ({val}) => {
+        return(
+            <button
+                className="keyboardBtn"
+                onClick={() => handleKeyboardClick(val)}
+            >
+                {val}
+            </button>
+        )
+    }
+
+    function generateKeyboard(arr) {
+        let resultArr = [];
+        arr.map((it, idx) => {
+            resultArr.push(<KeyboardBtn val={it} key={idx} />)
+        })
+        return resultArr;
+    }
+
     return (
         <article className="keyboard">
-            <section>
-                <button
-                    className="keyboardBtn"
-                    onClick={() => handleKeyboardClick("ㅂ")}
-                >
-                    ㅂ
-                </button>
-            </section>
+            <div className="keyboardLine">
+                {generateKeyboard(keyboardArr1)}
+            </div>
+            <div className="keyboardLine">
+                {generateKeyboard(keyboardArr2)}
+            </div>
+            <div className="keyboardLine">
+                {generateKeyboard(keyboardArr3)}
+            </div>
         </article>
     );
 };
