26457e8691dbcf1832041a8dc2e6dfd4bc5614d0
sungwon <sungwon_cho@fursys.com>
2023-05-07 18:48:00 +0900

    EZ모드 꽃잎배경 적용, todo:css값 변수로 받아오기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import "./App.css";
 import Main from "./pages/Main";
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import { initialState, reducer } from "./reducer/reducer";
 
 function App() {
     const [values, dispatch] = useReducer(reducer, initialState);
+    const blossomLeaf = () => {
+        const result = [];
+        for (let i = 0; i < 7; i++) {
+            const bgImg = `url("/public/img/blossom_leaf_${i + 1}.png");`;
+            const animation = `snowfall ${
+                5 + Math.random(10)
+            }s linear infinite`;
+            const animationDelay = Math.random(10);
+            const styleArr = `
+            --bg-img=${bgImg} 
+            --size=${Math.random() * (30 - 15) + 15} + 'px'
+            --left-ini=${Math.random(20) - 10} 
+            --left-end=${Math.random(20) - 10}
+            --left=${Math.random(100)}
+            --animation=${10 + Math.random(10)}
+            --animation-delay=${Math.random(10)}
+            `;
+            result.push(
+                <div
+                    class={["blossomLeaf", `img${i + 1}`].join(" ")}
+                    /* style={{
+                        backgroundImage: bgImg,
+                        animation: animation,
+                        animationDelay: animationDelay,
+                    }} */
+                ></div>
+            );
+        }
+        return result;
+    };
+
+    useEffect(() => {});
+
     return (
         <div className="App">
-            <div
-                className={[
-                    "bgImg",
-                    "snowflake",
-                    values.EZMode ? "EZMode" : "HARDMode",
-                ].join(" ")}
-            ></div>
-            <div
-                className={[
-                    "bgImg",
-                    "snowflake2",
-                    values.EZMode ? "EZMode" : "HARDMode",
-                ].join(" ")}
-            ></div>
+            {blossomLeaf()}
             <Main />
         </div>
     );
