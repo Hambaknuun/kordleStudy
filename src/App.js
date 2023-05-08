2fd6611defa4b95926a576950b656db32562953b
sungwon <sungwon_cho@fursys.com>
2023-05-08 22:31:46 +0900

    개발코드 제거, css 수정

--- src/App.js
+++ src/App.js
@@ -8,21 +8,13 @@ function App() {
     const blossomLeaf = () => {
         const result = [];
         for (let i = 0; i < 7; i++) {
-            const bgImg = `url("${
+            /* const bgImg = `url("${
                 process.env.PUBLIC_URL
-            }/public/img/blossom_leaf_${i + 1}.png")`;
+            }/public/img/blossom_leaf_${i + 1}.png")`; */
+            const bgImg = `url("public/img/blossom_leaf_${i + 1}.png")`;
             const animation = `snowfall ${
                 5 + Math.random(10)
             }s linear infinite`;
-            const animationDelay = Math.random(10);
-            const styleArr = `
-            --size=${Math.random() * (30 - 15) + 15} + 'px'
-            --left-ini=${Math.random(20) - 10} 
-            --left-end=${Math.random(20) - 10}
-            --left=${Math.random(100)}
-            --animation=${10 + Math.random(10)}
-            --animation-delay=${Math.random(10)}
-            `;
             result.push(
                 <div
                     className={["blossomLeaf", `img${i + 1}`].join(" ")}
